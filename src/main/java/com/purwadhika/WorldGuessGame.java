97180fb49247f745b0cea84951c8a63c7ba134cd
purbaronny <95927137+purbaronny@users.noreply.github.com>
2025-02-07 11:38:51 +0700

    feat(WorldGuessGame) initial

---
+++ src/main/java/com/purwadhika/WorldGuessGame.java
@@ -0,0 +1,28 @@
+package com.purwadhika;
+
+public class WorldGuessGame {
+
+    private String randomWord;
+
+    public WorldGuessGame() {}
+
+    public String selectRandomWord() {
+        throw new UnsupportedOperationException("selectRandomWord is not supported");
+    }
+
+    public void hide(String word) {
+        throw new UnsupportedOperationException("hide is not supported");
+    }
+
+    public boolean isGuessCorrect(String word, char guess) {
+        throw new UnsupportedOperationException("isGuessCorrect is not supported");
+    }
+
+    public void updateHiddenWord(String word, String hiddenWord, char guess) {
+        throw new UnsupportedOperationException("updateHiddenWord is not supported");
+    }
+
+    public void displayGameResult(String wordToGuess, String hiddenWordm, int attemptsLeft) {
+        throw new UnsupportedOperationException("updateHiddenWord is not supported");
+    }
+}
